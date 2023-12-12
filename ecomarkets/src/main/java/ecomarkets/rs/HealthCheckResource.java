fe78bbe8d6a9fdba797da2f86a22d144b9b0b447
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-12-12 13:27:00 +0000

    health check

---
+++ ecomarkets/src/main/java/ecomarkets/rs/HealthCheckResource.java
@@ -0,0 +1,43 @@
+package ecomarkets.rs;
+
+import java.util.Collection;
+import java.util.List;
+
+import ecomarkets.domain.core.Tenant;
+import ecomarkets.domain.core.basket.Basket;
+import ecomarkets.domain.core.basket.BasketItem;
+import ecomarkets.domain.core.partner.PartnerId;
+import io.quarkus.panache.common.Sort;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/_hc")
+public class BasketResource {
+    @Inject
+    DataSource ds;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Sttring> getHealthCheck() {
+        var hc = Map.of("datasrouce.isValid", isValidDatasource())
+        return result;
+    }
+
+    public String isValidDatasource()[
+        try(var conn = ds.getConnection()){
+            return "" + conn.isValid(30);
+        }catch {Exception e}{
+            return e.getMessage();
+        }
+    ]
+
+}
