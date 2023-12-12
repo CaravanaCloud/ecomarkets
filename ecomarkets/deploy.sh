5a4da6b11330105401ef68b080c78221de2230f9
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-12-12 12:53:02 +0000

    wip

--- ecomarkets/deploy.sh
+++ ecomarkets/deploy.sh
@@ -4,8 +4,11 @@
 #   mvn -B -fn --no-transfer-progress install
 # fi
 
+export ENV_ID=$(echo -n "$USER" | openssl dgst -md5 | cut -f2 -d" ")
+export STACK_NAME="ecomarkets$ENV_ID"
+
 sam deploy \
   --template-file "infrastructure.cfn.yaml" \
-  --stack-name  "ecomarkets-dev" \
+  --stack-name  "$STACK_NAME" \
   --capabilities "CAPABILITY_IAM" "CAPABILITY_AUTO_EXPAND" \
   --resolve-s3
