c2f21ef65e2d373bda1bcafc337fa0e5f025252d
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-12-13 12:26:53 +0000

    wip

--- ecomarkets/delete.sh
+++ ecomarkets/delete.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
-
 DEFAULT_ENV_ID=$(echo -n "$USER" | openssl dgst -md5 | cut -f2 -d" " | cut -c1-10)
 CWD_NAME=$(basename $PWD)
-ENV_ID=${ENV_ID:-"${CWD_NAME}${ENV_ID}"}
+ENV_ID=${ENV_ID:-"${CWD_NAME}${DEFAULT_ENV_ID}"}
 STACK_NAME="$ENV_ID"
 
 echo "Deleting [$STACK_NAME]"
