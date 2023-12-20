cb82f9fdd153ac25da1dd10215448284f5faa62b
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-12-20 21:00:32 +0000

    svelte wip

--- ecomarkets-app/src/routes/+page.svelte
+++ ecomarkets-app/src/routes/+page.svelte
@@ -1,8 +1,18 @@
-<script></script>
+<script>
+    let title = "Welcome to EcoMarkets!"
+    let time = "";
+    const getTime = () => { 
+        time = "" + new Date().toLocaleTimeString()
+        console.debug("Time is", time)
+    }
+    getTime()
+</script>
 
 <div class="index">
-    <h2>Welcome to EcoMarkets!</h2>
+    <h2>{title}</h2>
     <p>Eat better, live better.</p>
+    <p>Time is {time}</p>
+    <button on:click={getTime}>Time is {time}</button>
 </div>
 
 <style>
