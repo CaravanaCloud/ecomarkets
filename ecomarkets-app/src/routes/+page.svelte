797bda8ffa081390912ad951a2194ad05af39c7b
Julio Faerman <jufaerma@redhat.com>
2023-12-20 19:59:16 +0000

    wip

--- ecomarkets-app/src/routes/+page.svelte
+++ ecomarkets-app/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script></script>
+
+<div class="index">
+    <h2>Welcome to EcoMarkets!</h2>
+    <p>Eat better, live better.</p>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
