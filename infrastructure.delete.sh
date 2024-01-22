8911387c91fd0f9dc34463e91cfd4080edbdbb20
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-01-22 23:04:03 +0000

    wip

--- infrastructure.delete.sh
+++ infrastructure.delete.sh
@@ -5,18 +5,18 @@ ENV_ID=${ENV_ID:-$USER}
 echo "deleting infrastructure for $ENV_ID"
 
 
-sam delete --no-prompts --stack-name "$ENV_ID-net" 
-
-sam delete --no-prompts --stack-name "$ENV_ID-params" 
-
-sam delete --no-prompts --stack-name "$ENV_ID-db" 
-
-sam delete --no-prompts --stack-name "$ENV_ID-api-fn" 
-
-sam delete --no-prompts --stack-name "$ENV_ID-api-dns" 
+sam delete --no-prompts --stack-name "$ENV_ID-cdn" 
 
 sam delete --no-prompts --stack-name "$ENV_ID-eks" 
 
-sam delete --no-prompts --stack-name "$ENV_ID-cdn" 
+sam delete --no-prompts --stack-name "$ENV_ID-api-dns" 
+
+sam delete --no-prompts --stack-name "$ENV_ID-api-fn" 
+
+sam delete --no-prompts --stack-name "$ENV_ID-db" 
+
+sam delete --no-prompts --stack-name "$ENV_ID-net" 
+
+sam delete --no-prompts --stack-name "$ENV_ID-params" 
 
 echo "done delete "
