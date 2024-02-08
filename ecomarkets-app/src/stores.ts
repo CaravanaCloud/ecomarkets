f203e788508019af37d7f502623786190f97d935
Wallys Ferreira <wallysfsilva@gmail.com>
2024-02-08 18:32:40 +0000

    chore: add EOF

--- ecomarkets-app/src/stores.ts
+++ ecomarkets-app/src/stores.ts
@@ -2,5 +2,5 @@ import { writable } from 'svelte/store';
 
 export const cat_store = writable([])
 export const selected_category = writable("Bebidas")
-export const selected_product = writable()
 export const show_info = writable(false)
+export const selected_product = writable()
