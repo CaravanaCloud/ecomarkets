1391720b7756be538be2ebfaee55366c0dbe802f
Wallys Ferreira <wallysfsilva@gmail.com>
2024-02-08 01:13:03 +0000

    feat: init CategoriesBar component

---
+++ ecomarkets-app/src/routes/produtos/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import CategoriesBar from '$lib/CategoriesBar.svelte';
+
+  let categories = [
+    {name: "Testando", is_selected: true},
+    {name: "Mais um teste", is_selected: false},
+    {name: "É um teste", is_selected: false},
+    {name: "Teste", is_selected: false},
+  ]
+</script>
+
+<CategoriesBar {categories} />
