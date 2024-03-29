fe41a72f2f90a1fd1e49b66697063f82d4311592
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-03-29 13:20:35 +0000

    wip

--- ecomarkets-api/src/main/java/ecomarkets/domain/core/product/image/Tag.java
+++
@@ -1,7 +0,0 @@
-package ecomarkets.domain.core.product.image;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-
-@Embeddable
-public record Tag (@Column(name = "tag_key") String key, String value){}
--- ecomarkets-api/src/main/java/ecomarkets/domain/notification/email/EmailTemplate.java
+++
@@ -1,7 +0,0 @@
-package ecomarkets.domain.notification.email;
-
-import java.math.BigDecimal;
-
-public interface EmailTemplate {
-    String getBody(String partnerName, Long basketId, BigDecimal paymentValue, String status);
-}
