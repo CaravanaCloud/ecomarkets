e21fbc220085850760fd800470caff969058f8b6
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-04-01 18:04:21 +0200

    wip

--- infra/tf/env-auth/tf-apply.sh
+++ infra/tf/env-auth/tf-apply.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+./#!/bin/bash
 set -x 
+
+export TF_VAR_env_id=$(terraform -chdir=../env-security output -raw env_id)
 
 terraform apply -auto-approve 
--- infra/tf/env-auth/variables.tf
+++ infra/tf/env-auth/variables.tf
@@ -1,10 +1,5 @@
-variable env_id {
-  default = "ecomarkets_dev"
-}
-
-variable aws_region {
-  default = "us-east-1"
-}
+variable env_id {}
+variable aws_region {}
 
 variable "google_client_id" {
   description = "The Client ID from Google"
