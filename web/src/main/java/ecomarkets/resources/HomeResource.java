242d2a9b5c12f359b21b99f706b4433d505fd266
Eric Rosetti Lessa <ericrlessa@gmail.com>
2024-04-05 10:27:33 -0300

    wip

--- web/src/main/java/ecomarkets/resources/HomeResource.java
+++ web/src/main/java/ecomarkets/resources/HomeResource.java
@@ -1,12 +1,17 @@
 package ecomarkets.resources;
 
+import ecomarkets.core.domain.core.product.Product;
+import ecomarkets.core.domain.core.product.category.Category;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
 
 @Path("/")
 public class HomeResource {
@@ -15,14 +20,23 @@ public class HomeResource {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
+    @Transactional
     public TemplateInstance get() {
-        return index.instance();
+        return fillData();
     }
 
     @GET
     @Path("/index")
     @Produces(MediaType.TEXT_HTML)
+    @Transactional
     public TemplateInstance getIndex() {
-        return index.instance();
+        return fillData();
+    }
+
+
+    private TemplateInstance fillData(){
+        List<Product> productList = Product.findAll().list();
+        List<Category> categories = Category.findAll().list();
+        return index.data("products", productList, "categories", categories);
     }
 }
